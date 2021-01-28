a25d54629d7732f76a12de46aed9c7b66b912694
subrotonpi <subroto.npi@gmail.com>
2021-01-28 18:28:48 +0600

    -

--- so_papers/so_papers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('cluster/', include('cluster.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('cluster.urls')),
-    
-]
